8630a45ea08f60379e834c4cff61ad53ed36f1d8
creativecove <lorenzo.amato@stu.cvccworks.edu>
2025-04-04 14:10:25 -0400

    Add files via upload

---
+++ projects/java3/chapter/js03_txt.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Lorenzo A
+     Date: 4/1/2025
+
+     Filename: js03.js
+ */
